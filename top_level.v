fd07e67eb1e1bd884d88fd65ff300a1ce4a16fc0
fuse <m13.f98@gmail.com>
2021-11-08 21:50:34 +0330

    save top level

--- top_level.v
+++ top_level.v
@@ -12,18 +12,18 @@ module top_level(input rst, clk);
     assign Dest_wb = 4'b0;
     assign status = 4'b1111;
 
-    wire wb_enable_exec, mem_read_enable, mem_write_enable, branch_enable, S, immidiate
+    wire wb_enable_exec, mem_read_enable, mem_write_enable, branch_enable, S, immidiate;
     wire [3:0] exec_cmd, Status, Dest;
-    wire [31:0] Val_Rn, Val_Rm,
+    wire [31:0] Val_Rn, Val_Rm;
     
-    wire [11:0] Shift_operand, 
-    wire [23:0] Signed_immidiate_24,
-    
+    wire [11:0] Shift_operand;
+    wire [23:0] Signed_immidiate_24;
+  
     IF_Module if_module(rst, clk, freeze, flush, Branch_taken, BranchAddr, pc_1, Instruction);
     ID_Module id_module(
-        rst, clk, freeze, flush, Branch_taken, BranchAddr, pc_1, Instruction, Result_WB, Dest_wb, status,
-        wb_enable_exec, mem_read_enable, mem_write_enable, branch_enable, 
-        exec_cmd, Val_Rn, Val_Rm, immidiate,
+        rst, clk, freeze, flush, WBEnable, pc_1, Instruction, Result_WB, Dest_wb, status,
+        wb_enable_exec, mem_read_enable, mem_write_enable, branch_enable, S, 
+        exec_cmd, pc_2,  Val_Rn, Val_Rm, immidiate,
         Shift_operand, Signed_immidiate_24, Dest, Status
     );
     EXE_Module exe_module(rst, clk, pc_2, pc_3);
